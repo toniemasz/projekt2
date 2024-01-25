ef7df687a4fce1d104a3c0d009caccfed2d9e695
toniemasz <tomaszf600@gmail.com>
2024-01-25 21:53:50 +0100

    KOD

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,10 @@
 public class Runner {
 
     public static void main(String[] args) throws InterruptedException {
-        // Create instances of the server and client
+
         CurrencyConverterServer server = new CurrencyConverterServer();
         CurrencyConverterClient client = new CurrencyConverterClient();
 
-        // Create and start threads for server and client
         Thread serverThread = new Thread(() -> server.connectToServer());
         Thread clientThread = new Thread(() -> client.connectToClient());
 
